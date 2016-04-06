cde502b336c912ae0681339800e184b2224c7f5e
Diarmaid Lindsay <diarmaid.lindsay@gmail.com>
2016-04-06 12:00:36 +0900

    Initial commit

---
+++ app/src/main/java/pulseanddecibels/jp/buzbiznew/activity/MainActivity.java
@@ -0,0 +1,21 @@
+package pulseanddecibels.jp.buzbiznew.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import pulseanddecibels.jp.buzbiznew.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton dialerButton = (FloatingActionButton) findViewById(R.id.fab);
+    }
+}
